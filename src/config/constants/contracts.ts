203e3ad78476446d0846f7eebd74122a1f37f077
octahedron22 <nmn.amberg@gmail.com>
2021-05-17 20:34:26 +0200

    first

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,34 @@
+export default {
+  cake: {
+    56: '0x57067A6BD75c0E95a6A5f158455926e43E79BeB0',
+    97: '',
+  },
+  xblzd: {
+    56: '0x367CdDA266ADa588d380C7B970244434e4Dde790',
+    97: '',
+  },
+  masterChef: {
+    56: '0x740A1c61A17BC7F498e33251dAADc80ECB27E05A',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
